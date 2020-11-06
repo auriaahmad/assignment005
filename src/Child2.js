84d6878f99d188f93f4bfde43fa77b75a2e19259
auria <oryaofficial@gmail.com>
2020-11-05 20:25:36 -0800

    contextAPI and Reducer

---
+++ src/Child2.js
@@ -0,0 +1,17 @@
+import React, {useReducer} from 'react';
+import CounterReducer from './CounterReducer'; 
+
+const Child2 = () =>{
+    //lets make two variables 
+    let [state, dispatch] = useReducer(CounterReducer, 1);
+    console.log(state)
+    return(
+        <div>
+            <h2>this is second child with counter reducer</h2>
+            <h3>value of reducer state is : {state}</h3>
+            <button onClick={()=>dispatch('INCREMENT')}>
+                increment reducer</button>
+        </div>
+    )
+}
+export default Child2;
---
+++ src/CounterContext.js
@@ -0,0 +1,5 @@
+import React, {createContext} from 'react'; // importing this hook from react
+
+const counterContext = createContext(0); // context creation here
+
+export default counterContext;
